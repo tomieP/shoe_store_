3c34553551ef7cb8d8f63741369fa8020da84eca
tomieP <nhphuc282@gmail.com>
2025-12-31 08:00:23 +0700

    comit_code_service

---
+++ src/services/code_service.py
@@ -0,0 +1,13 @@
+"""
+tao code sp theo cau truc
+
+KIỄU_HÃNG_MOTANGAN_SIZE
+
+KIỄU:   GIÀY: G
+        DÉP: D
+HÃNG: <TÊN HÃNG>
+MOTANGAN: <MÔ TẢ NGẮN VỀ SẢN PHẨM, CÓ THỂ LÀ TÊN MẪU>
+SIZE: SIZE SẢN PHẨM, LÀ SỐ HOẶC KÍ TỰ LA MÃ
+
+vd: D_2A_TO-ONG_XXL
+"""
