9933de6b9e6bc3e290d608dcc0a848a4429a1dbc
tomieP <nhphuc282@gmail.com>
2026-02-04 17:17:19 +0700

    refactor database schema: update table definitions and constraints

--- src/database.py
+++ src/database.py
@@ -15,70 +15,100 @@ class Database:
             conn.execute("PRAGMA foreign_keys = ON;")
             cursor = conn.cursor()
 
+        # #CUSTOMERS TABLE
+        #     cursor.execute('''
+        #                    CREATE TABLE IF NOT EXISTS customers(
+        #                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+        #                    customer_code TEXT UNIQUE NOT NULL,
+        #                    name TEXT NOT NULL,
+        #                    phone TEXT,
+        #                    email TEXT,
+        #                    total_purchases REAL DEAFAULT 0,
+        #                    note TEXT,
+        #                    is_active INTEGER DEFAULT 1 CHECK(is_active IN (0,1)),
+        #                    point INTEGER DEFAULT 0,
+        #                    created_at TEXT NOT NULL,
+        #                    updated_at TEXT NOT NULL
+        #                    )
+        #                    ''')
         #PRODUCTS TABLE
             cursor.execute('''
                 CREATE TABLE IF NOT EXISTS products(
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        code TEXT UNIQUE NOT NULL,
+                        product_code TEXT UNIQUE NOT NULL,
                         category TEXT NOT NULL,
                         brand TEXT,
+                        cost_price REAL NOT NULL,
                         price REAL NOT NULL,
                         size TEXT NOT NULL,
-                        quantity INTEGER DEFAULT 0 CHECK (quantity > 0),
+                        color TEXT,
+                        quantity INTEGER NOT NULL CHECK (quantity > 0)),
+                        min_quantity INTEGER DEFAULT 5,
                         name TEXT,
                         imagePath TEXT NOT NULL,
                         QRPath TEXT NOT NULL,
-                        is_active INTEGER NOT NULL CHECK(is_active IN (0,1)),
+                        is_active INTEGER DEFAULT 1 CHECK(is_active IN (0,1)),
                         description TEXT,                         
                         updated_at TEXT NOT NULL,
-                        created_at TEXT NOT NULL
+                        created_at TEXT NOT NULL,
+                           
+                        UNIQUE(code, size, brand, color)
                         )
                         ''')
         #INVOICE TABLE
             cursor.execute('''
                 CREATE TABLE IF NOT EXISTS invoices(
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        method TEXT NOT NULL,
+                        invoice_code TEXT UNIQUE NOT NULL,
+                        payment_method TEXT NOT NULL CHECK(payment_method IN (cash, banking)),
                         total_amount REAL NOT NULL,
                         note TEXT,
                         created_at TEXT NOT NULL
+                        updated_at TEXT NOT NULL
                         )
                         ''')
         #INVOICE DETAILS TABLE
             cursor.execute('''
                 CREATE TABLE IF NOT EXISTS invoice_details(
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        id_invoice INTEGER NOT NULL,
-                        id_product INTEGER NOT NULL,
-                           
+                        invoice_id INTEGER NOT NULL,
+                        product_id INTEGER NOT NULL,
+                        product_code TEXT NOT NULL,
                         name TEXT NOT NULL,
+                        color TEXT,
                         size TEXT NOT NULL,
                         price REAL NOT NULL,
                         quantity INTEGER NOT NULL CHECK (quantity >= 0),
                         sub_total REAL NOT NULL,
                         note TEXT,
-                        FOREIGN KEY (id_invoice) REFERENCES invoices(id),
-                        FOREIGN KEY (id_product) REFERENCES products(id)
+                        FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+                        FOREIGN KEY (product_id) REFERENCES products(id),
+                        UNIQUE(invoice_id, product_id, size, color))
                         )
                         '''
                         )
-        #DAILY_RP TABLE
+        #DAILY_REPORTS TABLE
             cursor.execute('''
-                CREATE TABLE IF NOT EXISTS daily_rp(
+                CREATE TABLE IF NOT EXISTS daily_reports(
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        total_units_sold INTEGER NOT NULL  CHECK (total_units_sold >= 0),
-                        invoice_total_amount INTEGER NOT NULL  CHECK (invoice_total_amount >= 0),                           
-                        daily_revenue REAL NOT NULL CHECK (daily_revenue >= 0),
+                        report_date TEXT NOT NULL UNIQUE,
+                        total_units_sold INTEGER DEFAULT 0,
+                        total_invoices INTEGER DEFAULT 0,                           
+                        total_sales_amount REAL DEFAULT 0,
+                        total_inventory_value REAL DEFAULT 0,
+                        top_product_json TEXT,
                         note TEXT,
                         created_at TEXT NOT NULL
+                        updated_at TEXT NOT NULL
                         )
                         ''')
-        #IMPORT ORDER
+        #IMPORT ORDERS TABLE
             cursor.execute('''
-                CREATE TABLE IF NOT EXISTS import_order(
+                CREATE TABLE IF NOT EXISTS import_orders(
                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                           import_code TEXT UNIQUE NOT NULL,
                            supplier_name TEXT,
-                           status TEXT NOT NULL CHECK (status IN ("WAITING", "FINISHED")),
+                           status TEXT NOT NULL CHECK (status IN ("CANCELED", "WAITING", "FINISHED")),
                            note TEXT,
                            shipping_fee REAL CHECK (shipping_fee >= 0),
                            received_at TEXT NOT NULL,
@@ -89,18 +119,23 @@ class Database:
             cursor.execute('''
                 CREATE TABLE IF NOT EXISTS import_items(
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        id_order INTEGER NOT NULL,
-                        id_product INTEGER NOT NULL,
-                           
-                        name TEXT NOT NULL,
+                        order_id INTEGER NOT NULL,
+                        product_id INTEGER NOT NULL,
+                        
+                        product_code TEXT NOT NULL,
+                        product_name TEXT NOT NULL,
                         size TEXT NOT NULL,
+                        color TEXT,
                         quantity INTEGER NOT NULL CHECK (quantity > 0),
                         unit_cost REAL NOT NULL CHECK (unit_cost > 0),
+                        total_cost REAL NOT NULL CHECK (total_cost > 0),
                         
                         created_at TEXT NOT NULL,
 
-                        FOREIGN KEY (id_product) REFERENCES products(id),
-                        FOREIGN KEY (id_order) REFERENCES import_order(id)
+                        FOREIGN KEY (import_order_id) REFERENCES import_orders(id) ON DELETE CASCADE,
+                        FOREIGN KEY (product_id) REFERENCES products(id),
+                    
+                        UNIQUE(import_order_id, product_id, size)
                                             
                         )
                         ''')
